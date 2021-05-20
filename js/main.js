9a52aac1f8ea1704458280f304f16c851bf4ec68
eulinp <eulin-palma@hotmail.com>
2021-05-19 20:15:50 -0430

    Agregada sección ser creador

--- js/main.js
+++ js/main.js
@@ -57,3 +57,158 @@ for(let i = 0; i < tog.length; i++){
 // /*BOX*/
 // gsap.from('.box__medium', {opacity: 0, duration: 1.5, delay:1.3, y: -50})
 // gsap.from('.box__small', {opacity: -1, duration: 1.5, delay:1.4, y: -50})
+
+
+
+
+
+////////////////////GRAFICO////////////////////////////////
+  
+      // anychart.onDocumentReady(function grafico () {
+      //   // create data set
+      //   var dataSet = anychart.data.set(getData(aud.value, cuo.value));
+        
+        
+       
+
+      //   // map data for the first series, take x from the zero column and value from the first column of data set
+      //   var firstSeriesData = dataSet.mapAs({ x: 0, value: 1 });
+  
+      //   // map data for the second series, take x from the zero column and value from the second column of data set
+      //   var secondSeriesData = dataSet.mapAs({ x: 0, value: 2 });
+  
+      //   // create bar chart
+      //   var chart = anychart.bar();
+  
+      //   // turn on chart animation
+      //   chart.animation(true);
+  
+      //   // set padding
+      //   chart.padding([10, 20, 5, 20]);
+  
+      //   // force chart to stack values by Y scale.
+      //   chart.yScale().stackMode('value');
+  
+      //   // format y axis labels so they are always positive
+      //   chart
+      //     .yAxis()
+      //     .labels()
+      //     .format(function () {
+      //       return Math.abs(this.value).toLocaleString();
+      //     });
+  
+      //   // set title for Y-axis
+      //   chart.yAxis(0).title('');
+  
+      //   // allow labels to overlap
+      //   chart.xAxis(0).overlapMode('allow-overlap');
+  
+      //   // turn on extra axis for the symmetry
+      //   chart
+      //     .xAxis(1)
+      //     .enabled(true)
+      //     .orientation('right')
+      //     .overlapMode('allow-overlap');
+  
+      //   // set chart title text
+      //   chart.title('Simulador de ganancias');
+  
+      //   chart.interactivity().hoverMode('by-x');
+  
+      //   chart
+      //     .tooltip()
+      //     .title(false)
+      //     .separator(false)
+      //     .displayMode('separated')
+      //     .positionMode('point')
+      //     .useHtml(true)
+      //     .fontSize(12)
+      //     .offsetX(5)
+      //     .offsetY(0)
+      //     .format(function () {
+      //       return (
+      //         '<span style="color: #D9D9D9">$</span>' +
+      //         Math.abs(this.value).toLocaleString()
+      //       );
+      //     });
+  
+      //   // temp variable to store series instance
+      //   var series;
+  
+      //   // create first series with mapped data
+      //   series = chart.bar(firstSeriesData);
+      //   series.name('Cuota del socio').color('#7135F2');
+      //   series.tooltip().position('right').anchor('left-center');
+  
+      //   // create second series with mapped data
+      //   series = chart.bar(secondSeriesData);
+      //   series.name('Tamaño de audiencia').color('#05ADD2');
+      //   series.tooltip().position('left').anchor('right-center');
+  
+      //   // turn on legend
+      //   chart
+      //     .legend()
+      //     .enabled(true)
+      //     .inverted(true)
+      //     .fontSize(13)
+      //     .padding([0, 0, 20, 0]);
+  
+      //   // set container id for the chart
+      //   chart.container('container');
+  
+      //   // initiate chart drawing
+      //   chart.draw();
+      // });
+  
+      // function getData(param1, param2) {
+      //   return [
+      //     ['', param1, -(param2)],
+      //   ];
+      // }
+    
+
+
+
+
+
+
+      
+      ////////////////////////SIMULADOR INTERACTIVO///////////////
+ 
+        let aud = document.querySelector("#audiencia"),
+            cuo = document.querySelector("#cuota");
+
+            aud.addEventListener('change', ()=> {
+                document.querySelector("#audienciaSpan").innerHTML = aud.value;
+                document.querySelector("#total").innerHTML = total();
+                () => {
+                  // append data
+                  dataSet.append({
+                
+                    // x value
+                    x: -20 + indexSetter,
+                
+                    // random value from 1 to 500
+                    value : Math.floor((Math.random() * 500)+ 1)
+                  });
+                
+                  // removes first point
+                  dataSet.remove(0);
+                  indexSetter++;
+                }, 
+                200
+            })
+
+            cuo.addEventListener('change', ()=> {
+                document.querySelector("#cuotaSpan").innerHTML = cuo.value;
+                document.querySelector("#total").innerHTML = total();
+
+            })
+
+
+            function total(){
+                return (Number(aud.value*cuo.value) - Number(aud.value*cuo.value)*10/100);
+            }
+
+
+
